9cee8ad57b15218c2f146cb3ec5ea4e50734495c
Robert Anderson <pwmrrob@outlook.com>
2024-02-27 19:19:03 -0500

    Messing with tailwind

--- next-drizzle-starter/src/app/components/CTA.tsx
+++ next-drizzle-starter/src/app/components/CTA.tsx
@@ -3,8 +3,8 @@ export default function CTA() {
         <section className="bg-white dark:bg-lime-400">
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                 <div className="max-w-screen-md">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"> We find what brings us together.</h2>
-                    <p className="mb-8 font-light text-gray-500 sm:text-xl dark:text-gray-400">Flowbite helps you connect with friends, family and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups, Watch and Marketplace.</p>
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black"> We find what brings us together.</h2>
+                    <p className="mb-8 font-light text-white sm:text-xl dark:text-white">Flowbite helps you connect with friends, family and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups, Watch and Marketplace.</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
                         <a href="#" className="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             Get started
